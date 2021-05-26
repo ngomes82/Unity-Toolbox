594bd1b17cb5881493fed2ab89bffff9eabb14f8
nick <nickgomes82@gmail.com>
2021-05-26 14:45:07 -0400

    Adding a simple sampling function

---
+++ Assets/Scripts/Utils/Math/MathUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MathUtils
+{
+    //Returns N samples of a 1-term mathmatical function over the given range.
+    public static List<float> SampleFunc(float start, float end, int nSamples, Func<float, float> func)
+    {
+        float interval = (end - start) / nSamples;
+        List<float> toReturn = new List<float>(nSamples);
+        for (int i = 0; i < nSamples; i++)
+        {
+            toReturn.Add(func(start + (i * interval)));
+        }
+
+        return toReturn;
+    }
+}
