9df79d3b9470a209e148b2b59b76c517fe5db0b3
nick <nickgomes82@gmail.com>
2021-05-26 14:46:56 -0400

    Adding a shuffle method to list extensions

---
+++ Assets/Scripts/Utils/Extensions/ListExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ListExtensions
+{
+    //Fisher-Yates Shuffle
+    public static void Shuffle<T>(this List<T> toShuffle)
+    {
+        int n = toShuffle.Count;
+        for (int i = 0; i < (n - 1); i++)
+        {
+            int r = i + Random.Range(0, n - i);
+            T t = toShuffle[r];
+            toShuffle[r] = toShuffle[i];
+            toShuffle[i] = t;
+        }
+    }
+}
