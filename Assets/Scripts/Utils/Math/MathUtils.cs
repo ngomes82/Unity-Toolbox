ffbea1ab4f876134288dba1b707c0376a2a235d8
nick <nickgomes82@gmail.com>
2021-05-27 14:36:33 -0400

    Adding some utility functions to convert between 1D <=> 2D array indicies

--- Assets/Scripts/Utils/Math/MathUtils.cs
+++ Assets/Scripts/Utils/Math/MathUtils.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 
 public static class MathUtils
 {
-    //Returns N samples of a 1-term mathmatical function over the given range.
+    /// <summary>
+    /// Returns N samples of a 1-term mathmatical function over the given range.
+    /// </summary>
+    /// <param name="start"></param>
+    /// <param name="end"></param>
+    /// <param name="nSamples"></param>
+    /// <param name="func"></param>
+    /// <returns></returns>
     public static List<float> SampleFunc(float start, float end, int nSamples, Func<float, float> func)
     {
         float interval = (end - start) / nSamples;
@@ -16,5 +23,30 @@ public static class MathUtils
         }
 
         return toReturn;
+    }
+
+    /// <summary>
+    /// Converts a linear index to a 2D index, left to right
+    /// </summary>
+    /// <param name="index1D"></param>
+    /// <param name="width"></param>
+    /// <param name="xIndex"></param>
+    /// <param name="yIndex"></param>
+    public static void Convert1DTo2DArrayIdx(int index1D, int width, out int xIndex, out int yIndex)
+    {
+        xIndex = index1D % width;
+        yIndex = index1D / width;
+    }
+
+    /// <summary>
+    /// Converts a 2D index into a linear index, left to right
+    /// </summary>
+    /// <param name="xIndex"></param>
+    /// <param name="yIndex"></param>
+    /// <param name="width"></param>
+    /// <returns></returns>
+    public static int Convert2DTo1DArrayIdx(int xIndex, int yIndex, int width)
+    {
+        return xIndex + (yIndex * width);
     }
 }
