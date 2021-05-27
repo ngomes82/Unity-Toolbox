d2b41163a9ca20f2be62c916ccfcef9c11c23a38
nick <nickgomes82@gmail.com>
2021-05-27 17:07:29 -0400

    adding some math util functions for picking random items in lists given their weights

--- Assets/Scripts/Utils/Math/MathUtils.cs
+++ Assets/Scripts/Utils/Math/MathUtils.cs
@@ -49,4 +49,41 @@ public static class MathUtils
     {
         return xIndex + (yIndex * width);
     }
+
+    /// <summary>
+    /// Picks a random item from a list given a second list with weights and the sum of the weights.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="items"></param>
+    /// <param name="weights"></param>
+    /// <param name="sum"></param>
+    /// <returns></returns>
+    public static T ChooseWithWeights<T>(List<T> items, List<float> weights, float sum)
+    {
+        Debug.Assert(items.Count == weights.Count, $"Items and weights are not the same sizes: {items.Count} != {weights.Count} ");
+
+        int index = ChooseWeightedArrayIndex(weights, sum);
+        return items[index];
+    }
+
+    /// <summary>
+    /// Picks a random index given a list of weights and the sum of weights.
+    /// </summary>
+    /// <param name="weights"></param>
+    /// <param name="sum"></param>
+    /// <returns></returns>
+    public static int ChooseWeightedArrayIndex(List<float> weights, float sum)
+    {
+        float randWeight = UnityEngine.Random.Range(0f, sum);
+        for (int i = 0; i < weights.Count; i++)
+        {
+            randWeight -= weights[i];
+            if (randWeight < 0f)
+            {
+                return i;
+            }
+        }
+
+        return weights.Count - 1;
+    }
 }
