2503d484f984301e633b07e3438d2502ec0ae58d
nick <nickgomes82@gmail.com>
2021-05-27 09:49:45 -0400

    Animator state change callback utilities based on this unity answers thread https://answers.unity.com/questions/1254566/is-there-a-way-to-get-a-statemachinebehaviour-on-a.html

---
+++ Assets/Scripts/Utils/Animation/AnimatorStateChangeCallback.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorStateChangeCallback : StateMachineBehaviour
+{
+    public enum StateChangeType : int
+    {
+        OnEnter = 0,
+        OnExit = 1
+    }
+
+    public Action[] callbacks = { null, null };
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        callbacks[0]?.Invoke();
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        callbacks[1]?.Invoke();
+        base.OnStateExit(animator, stateInfo, layerIndex);
+    }
+}
---
+++ Assets/Scripts/Utils/Extensions/AnimatorExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static AnimatorStateChangeCallback;
+
+public static class AnimatorExtensions
+{
+    public static bool TryRegisterStateChangeCallback(this Animator animator, string stateName, StateChangeType stateChangeType, Action callback)
+    {
+        bool registered = false;
+
+        int callbackIdx = (int)stateChangeType;
+
+        var stateCallbacks = animator.GetBehaviours<AnimatorStateChangeCallback>();
+        if (stateCallbacks.Length > 0)
+        {
+            foreach (var state in stateCallbacks)
+            {
+                if (state.name == stateName)
+                {
+                    state.callbacks[callbackIdx] = callback;
+                    registered = true;
+                }
+            }
+        }
+
+        return registered;
+    }
+}
