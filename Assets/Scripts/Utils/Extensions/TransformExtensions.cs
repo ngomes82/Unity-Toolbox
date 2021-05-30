efd5a18e9e871da5e4b803ca9b572eb6c364c97a
nick <nickgomes82@gmail.com>
2021-05-30 15:51:05 -0400

    adding some transform extension functions to make setting individual position values easier.

---
+++ Assets/Scripts/Utils/Extensions/TransformExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TransformExtensions
+{
+    public static Transform SetPosition(this Transform transform, float? x = null, float? y = null, float? z = null)
+    {
+        Vector3 newPos = transform.position;
+
+        if (x.HasValue) newPos.x = x.Value;
+        if (y.HasValue) newPos.y = y.Value;
+        if (z.HasValue) newPos.z = z.Value;
+        
+        transform.position = newPos;
+
+        return transform;
+    }
+}
