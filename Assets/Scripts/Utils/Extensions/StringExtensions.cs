b80a8dfea3ed21dc5deb5b31887695d0cd21d860
nick <nickgomes82@gmail.com>
2021-06-01 22:19:42 -0400

    adding some string extensions and utility functions

---
+++ Assets/Scripts/Utils/Extensions/StringExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class StringExtensions
+{
+    public static bool Contains(this string source, string value, StringComparison comparisonType)
+    {
+        return source.IndexOf(value, comparisonType) >= 0;
+    }
+
+    public static bool FuzzyContains(this string source, string value, int distance = 1)
+    {
+        return StringUtils.LevenshteinSubstringDistance(value, source) <= distance;
+    }
+
+    public static bool ContainsAll(this string source, string value, char separator, bool fuzzy = false, int distance = 1, bool fuzzyNumbers = false)
+    {
+        string[] searchStrings = value.Split(separator);
+        bool[] containsNumbers = new bool[searchStrings.Length];
+
+        if (fuzzy && !fuzzyNumbers)
+        {
+            for (int string_index = 0; string_index < searchStrings.Length; ++string_index)
+                containsNumbers[string_index] = searchStrings[string_index].Any(c => char.IsDigit(c));
+        }
+
+        bool allStringsFound = true;
+        for (int string_index = 0; string_index < searchStrings.Length; ++string_index)
+        {
+            string searchFor = searchStrings[string_index];
+
+            if (!source.Contains(searchFor, StringComparison.OrdinalIgnoreCase))
+            {
+                if (containsNumbers[string_index] || (fuzzy && !source.FuzzyContains(searchFor, distance)))
+                {
+                    allStringsFound = false;
+                    break;
+                }
+            }
+        }
+
+        return allStringsFound;
+    }
+}
---
+++ Assets/Scripts/Utils/StringUtils.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public static class StringUtils
+{
+    static public int LevenshteinSubstringDistance(string s0, string s1, int minFuzzyLength = 3)
+    {
+        var subString = s0.ToLower();
+        var searchIn = s1.ToLower();
+
+        if (subString.Length <= minFuzzyLength)
+            return searchIn.Contains(subString, StringComparison.OrdinalIgnoreCase) ? 0 : int.MaxValue;
+        if (searchIn.Length == 0)
+            return int.MaxValue;
+
+        int maxLength = Mathf.Max(subString.Length, searchIn.Length) + 1;
+        List<int> row0 = new List<int>(maxLength);
+
+        for (int i = 0; i < maxLength; ++i)
+            row0.Add(0);
+
+        for (int i = 0; i < subString.Length; ++i)
+        {
+            List<int> row1 = new List<int>();
+            row1.Add(i + 1);
+
+            for (int j = 0; j < searchIn.Length; ++j)
+            {
+                int cost = (subString[i] != searchIn[j]) ? 1 : 0;
+                row1.Add(Mathf.Min(row0[j + 1] + 1, row1[j] + 1, row0[j] + cost));
+            }
+
+            row0.Clear();
+            row0.AddRange(row1);
+        }
+
+        return Mathf.Min(row0.ToArray());
+    }
+}
