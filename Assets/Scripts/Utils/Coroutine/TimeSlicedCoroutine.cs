99284f65ec4daef301f1e1bd2854c8ff2fc059ab
nick <nickgomes82@gmail.com>
2021-06-24 08:58:53 -0400

    Adding a time sliced coroutine class that can be used to limit the execution time of a coroutine procedure

---
+++ Assets/Scripts/Utils/Coroutine/TimeSlicedCoroutine.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class TimeSlicedCoroutine : IEnumerator
+{
+    private float maxExecutionTime;
+    private IEnumerator coroutine;
+    private Stopwatch stopwatch;
+    public TimeSlicedCoroutine(float argMaxExecutionTime, IEnumerator argCoroutine)
+    {
+        maxExecutionTime = argMaxExecutionTime;
+        coroutine = argCoroutine;
+        stopwatch = new Stopwatch();
+    }
+
+    public object Current => null;
+
+    public bool MoveNext()
+    {
+        bool isRunning = true;
+        stopwatch.Start();
+        while (isRunning & stopwatch.Elapsed.TotalSeconds < maxExecutionTime)
+        {
+            isRunning = coroutine.MoveNext();
+        }
+
+        stopwatch.Reset();
+
+        return isRunning;
+    }
+
+    public void Reset()
+    {
+        stopwatch.Reset();
+    }
+}
