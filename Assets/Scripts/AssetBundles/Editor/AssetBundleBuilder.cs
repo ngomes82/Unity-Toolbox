ac30d36932942c14d671e1bd4f0b96e710b71fd9
nick <nickgomes82@gmail.com>
2021-12-20 11:51:22 -0500

    Almost have an s3 uploader working - need to fix it so signing matches what amazon is expecting.....

--- Assets/Scripts/AssetBundles/Editor/AssetBundleBuilder.cs
+++ Assets/Scripts/AssetBundles/Editor/AssetBundleBuilder.cs
@@ -6,9 +6,16 @@ using System.Collections;
 using System.IO;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
 
 public class AssetBundleBuilder : EditorWindow
 {
+    private static string rootUrl = string.Empty;
+    private static string userId = string.Empty;
+    private static string password = string.Empty;
+
     [MenuItem("Tools/Asset Bundle Builder")]
     static void ShowWindow()
     {
@@ -36,8 +43,21 @@ public class AssetBundleBuilder : EditorWindow
             File.WriteAllText(GetBundleBuildDir() + "/bundleHashes.json", JsonConvert.SerializeObject(bundleHashMap) );
         }
 
+        rootUrl = EditorGUILayout.TextField("Root Url: ", rootUrl);
+        userId = EditorGUILayout.TextField("User Id: ", userId);
+        password = EditorGUILayout.TextField("Password: ", password);
+
         if (GUILayout.Button("Upload All"))
         {
+            S3Uploader s3Uploader = new S3Uploader(rootUrl, userId, password);
+
+            var filesToUpload = Directory.GetFiles(GetBundleBuildDir());
+            for(int i=0; i < filesToUpload.Length; i++)
+            {
+                string name = Path.GetFileName(filesToUpload[i]);
+                s3Uploader.UploadFileToAWS3(name, filesToUpload[i]);
+            }
+
             //HTTP PUT bundles up on s3 or firestore
             //https://medium.com/xrlo-extended-reality-lowdown/uploading-to-aws-from-unity-5e7de2c80fce
         }
@@ -53,6 +73,84 @@ public class AssetBundleBuilder : EditorWindow
         }
 
         return outputPath;
+    }
+}
+
+public class S3Uploader
+{
+    private string awsBucketName = "MyTestBucket";
+    private string awsAccessKey = "XXXxxxXXXxxx";
+    private string awsSecretKey = "XXXXxxxxxxXXXxxxXXX";
+    private string awsURLBaseVirtual = "";
+
+    public S3Uploader(string bucketName, string accessKey, string secretKey)
+    {
+        awsAccessKey = accessKey;
+        awsSecretKey = secretKey;
+        awsBucketName = bucketName;
+
+        awsURLBaseVirtual = "http://" +
+           awsBucketName +
+           ".s3.amazonaws.com/";
+    }
+    public void UploadFileToAWS3(string FileName, string FilePath)
+    {
+        string region = "us-east-2";
+        string longDate = System.DateTime.UtcNow.ToString("yyyyMMdd'T'HHmmss'Z'");
+        string shortDate = System.DateTime.UtcNow.ToString("yyyyMMdd");
+        string signature = Convert.ToBase64String(getSignatureKey(awsSecretKey, shortDate, region, "s3"));
+
+        string url = $"http://{awsBucketName}.s3.amazonaws.com/{FileName}";
+        string authHeader = $"AWS4-HMAC-SHA256 Credential={awsAccessKey}/{shortDate}/{region}/s3/aws4_request," +
+                            $"SignedHeaders=host;x-amz-date;x-amz-content-sha256," +
+                            $"Signature={signature}";
+
+        WebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+        webRequest.Method = "PUT";
+
+        webRequest.Headers.Add("Authorization", authHeader);
+        webRequest.Headers.Add("x-amz-content-sha256", "UNSIGNED-PAYLOAD");
+        webRequest.Headers.Add("x-amz-date", longDate);
+
+        byte[] fileRawBytes = File.ReadAllBytes(FilePath);
+        webRequest.ContentLength = fileRawBytes.Length;
+        
+
+
+        //Send the request
+        Stream S3Stream = webRequest.GetRequestStream();
+        S3Stream.Write(fileRawBytes, 0, fileRawBytes.Length);
+        
+        Debug.Log("Sent bytes: " +
+            webRequest.ContentLength +
+            ", for file: " +
+            FileName);
+
+        S3Stream.Close();
+
+        WebResponse response = webRequest.GetResponse();
+        
+        Debug.Log(response.ToString());
+    }
+
+    static byte[] HmacSHA256(String data, byte[] key)
+    {
+        String algorithm = "HmacSHA256";
+        KeyedHashAlgorithm kha = KeyedHashAlgorithm.Create(algorithm);
+        kha.Key = key;
+
+        return kha.ComputeHash(Encoding.UTF8.GetBytes(data));
+    }
+
+    static byte[] getSignatureKey(String secretKey, String dateStamp, String regionName, String serviceName)
+    {
+        byte[] kSecret = Encoding.UTF8.GetBytes(("AWS4" + secretKey).ToCharArray());
+        byte[] kDate = HmacSHA256(dateStamp, kSecret);
+        byte[] kRegion = HmacSHA256(regionName, kDate);
+        byte[] kService = HmacSHA256(serviceName, kRegion);
+        byte[] kSigning = HmacSHA256("aws4_request", kService);
+
+        return kSigning;
     }
 }
 #endif
